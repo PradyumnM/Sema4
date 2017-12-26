12e8b4106ab2a0635f34e38829e185a9630bea40
PradyumnM <32297668+PradyumnM@users.noreply.github.com>
2017-12-26 10:07:06 +0530

    Update XLReader.java

--- src/com/sema/util/XLReader.java
+++ src/com/sema/util/XLReader.java
@@ -4,176 +4,168 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Properties;
-
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import java.sql.*;
-
 
 public class XLReader {
 
 	public static void main(String[] args) throws IOException {
 
-    	Properties prop = new Properties();
+		Properties prop = new Properties();
 		InputStream input = null;
 		input = new FileInputStream("config.properties");
 		prop.load(input);
 
 		File folder = new File(prop.getProperty("filedir"));
-		String geneCatFile1= prop.getProperty("geneCatFile1");
-		String geneCatFile2= prop.getProperty("geneCatFile2");
-		String geneCatFile3= prop.getProperty("geneCatFile3");
-		String geneCatFile4= prop.getProperty("geneCatFile4");
-		
-		String geneCatName1= prop.getProperty("geneCatName1");
-		String geneCatName2= prop.getProperty("geneCatName2");
-		String geneCatName3= prop.getProperty("geneCatName3");
-		String geneCatName4= prop.getProperty("geneCatName4");
+		String geneCatFile1 = prop.getProperty("geneCatFile1");
+		String geneCatFile2 = prop.getProperty("geneCatFile2");
+		String geneCatFile3 = prop.getProperty("geneCatFile3");
+		String geneCatFile4 = prop.getProperty("geneCatFile4");
+
+		String geneCatName1 = prop.getProperty("geneCatName1");
+		String geneCatName2 = prop.getProperty("geneCatName2");
+		String geneCatName3 = prop.getProperty("geneCatName3");
+		String geneCatName4 = prop.getProperty("geneCatName4");
+		String targetFileName = prop.getProperty("targetfilecsv");
+		String startSqlFileName = prop.getProperty("startsqlfile");
+		String endSqlFileName = prop.getProperty("endsqlfile");
 		File[] listOfFiles = folder.listFiles();
-		
-		String line1="";
+
+		String line1 = "";
 		String extension = "";
 		String fileSql = prop.getProperty("fileSql");
-		StringBuffer fileBuff = new StringBuffer("");
 		StringBuffer colNameBuff = new StringBuffer("");
 		StringBuffer recordBuff = new StringBuffer("");
-		HashMap<String,String> geneSet = new HashMap<String,String>();
+		HashMap<String, String> geneSet = new HashMap<String, String>();
 		ArrayList<String> runCombIdList = new ArrayList<String>();
-		String temp ="";
+		String temp = "";
 		try {
-			BufferedWriter fw = new BufferedWriter(new FileWriter(fileSql)) ;
-			Class.forName("com.mysql.jdbc.Driver"); 
-			Connection con=DriverManager.getConnection(  
-					prop.getProperty("database"),prop.getProperty("dbuser"),prop.getProperty("dbpassword")); 
-			Statement stmt=con.createStatement();  
-			Statement catStmt = con.createStatement();
+			BufferedWriter fw = new BufferedWriter(new FileWriter(fileSql));
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(prop.getProperty("database"), prop.getProperty("dbuser"),
+					prop.getProperty("dbpassword"));
+			Statement stmt = con.createStatement();
 			Statement geneStmt = con.createStatement();
-			ResultSet crs=catStmt.executeQuery("SELECT * FROM genecategory;"); 
-			
+
 			for (File file : listOfFiles) {
 				BufferedReader br = new BufferedReader(new FileReader(file));
 				if (file.isFile()) {
 					int i = file.getName().lastIndexOf('.');
 					if (i > 0) {
-						extension = file.getName().substring(i+1);
+						extension = file.getName().substring(i + 1);
 					}
-					if(file.getName().equalsIgnoreCase("1.start.sql")) {
-						System.out.println("writing "+file.getName());
+					if (file.getName().equalsIgnoreCase(startSqlFileName)) {
+						System.out.println("writing " + file.getName());
 						while ((line1 = br.readLine()) != null) {
 							fw.write(line1);
 							fw.newLine();
-						
-						}//while end	
-					
+
+						} // while end
+
 					}
-				}//If file End
-				}
-			
+				} // If file End
+				br.close();
+			}
+
 			for (File file : listOfFiles) {
 				if (file.isFile()) {
 					int i = file.getName().lastIndexOf('.');
 					if (i > 0) {
-						extension = file.getName().substring(i+1);
+						extension = file.getName().substring(i + 1);
 					}
 
-
-					if(file.getName().substring(0,8).equalsIgnoreCase("geneList")) {
-						System.out.println("writing "+file.getName());
+					if (file.getName().substring(0, 8).equalsIgnoreCase("geneList")) {
+						System.out.println("writing " + file.getName());
 						BufferedReader br = new BufferedReader(new FileReader(file));
 						while ((line1 = br.readLine()) != null) {
-							//System.out.println(line1);
+							// System.out.println(line1);
 							if (file.getName().equalsIgnoreCase(geneCatFile1))
-							geneSet.put(line1,geneCatName1);
+								geneSet.put(line1, geneCatName1);
 							if (file.getName().equalsIgnoreCase(geneCatFile2))
-								geneSet.put(line1,geneCatName2);
+								geneSet.put(line1, geneCatName2);
 							if (file.getName().equalsIgnoreCase(geneCatFile3))
-								geneSet.put(line1,geneCatName3);
+								geneSet.put(line1, geneCatName3);
 							if (file.getName().equalsIgnoreCase(geneCatFile4))
-								geneSet.put(line1,geneCatName4);
-							/*fileBuff.append(line1);
-						fw.write(line1);
-						fw.newLine();*/
-
+								geneSet.put(line1, geneCatName4);
+							/*
+							 * fileBuff.append(line1); fw.write(line1); fw.newLine();
+							 */
 
 						}
-						br.close();	
+						br.close();
 					}
-
-
 
 				}
 
 			}
-//Writing to gene table
-			ResultSet rs=stmt.executeQuery("SELECT * FROM run_combination;");  
-			int flag1=0;
+			// Writing to gene table
+			ResultSet rs = stmt.executeQuery("SELECT * FROM run_combination;");
+			int flag1 = 0;
 			fw.newLine();
 			fw.write("insert into gene(`name`,`run_combination_id`) values");
-			while(rs.next())  
-			{
-			// System.out.println(rs.getInt(1)+"  "+rs.getInt(4)); 
-			temp= Integer.toString(rs.getInt(1));
-			runCombIdList.add(temp);
-			
-			for (String key : geneSet.keySet()) {
-				if(flag1==0) 
-			    {
-					fw.write("(");
-			flag1=1;
-		        }
-			else
-			{fw.write(",");
-				fw.newLine();
-				fw.write("(");
+			while (rs.next()) {
+				// System.out.println(rs.getInt(1)+" "+rs.getInt(4));
+				temp = Integer.toString(rs.getInt(1));
+				runCombIdList.add(temp);
+
+				for (String key : geneSet.keySet()) {
+					if (flag1 == 0) {
+						fw.write("(");
+						flag1 = 1;
+					} else {
+						fw.write(",");
+						fw.newLine();
+						fw.write("(");
+					}
+					// System.out.println(itr.next());
+					fw.write("'" + key + "'" + "," + temp + ")");
+
+				}
 			}
-				//System.out.println(itr.next());
-				fw.write(key+","+temp+")");
-				
-			}
-			} fw.write(";");
-//Writing to target table			
+			fw.write(";");
+			// Writing to target table
 			for (File file : listOfFiles) {
 
-				if(file.getName().equalsIgnoreCase("targetList.csv")) {
-					System.out.println("writing target file "+file.getName());
+				if (file.getName().equalsIgnoreCase(targetFileName)) {
+					System.out.println("writing target file " + file.getName());
 					BufferedReader br = new BufferedReader(new FileReader(file));
 					fw.newLine();
-					colNameBuff.append("INSERT INTO TARGET "+ "(");
-					colNameBuff.append(br.readLine()+",run_combination_id)"+" VALUES ");
+					colNameBuff.append("INSERT INTO target " + "(");
+					br.readLine();
+					colNameBuff.append("name,gene_id,exons,chromosome,start_index,end_index,q25_quartile,median,q75_quartile,run_combination_id)" + " VALUES ");
 					fw.write(colNameBuff.toString());
-					int flag=0;
+					int flag = 0;
+					String csvData[];
 					while ((line1 = br.readLine()) != null) {
 
-						for(int i=0;i<runCombIdList.size();i++)
-						{ 
-							if(flag==0) 
-							    {
-							recordBuff.append("(");
-							flag=1;
-						        }
-							else
-							{fw.write(",");
+						for (int i = 0; i < runCombIdList.size(); i++) {
+							if (flag == 0) {
+								recordBuff.append("(");
+								flag = 1;
+							} else {
+								fw.write(",");
 								fw.newLine();
-							recordBuff.append("(");
+								recordBuff.append("(");
 							}
-							
-							recordBuff.append(line1);
-							recordBuff.append(","+runCombIdList.get(i)+")");
+							csvData=line1.split(",");
+							for(int k=0;k<csvData.length;k++)
+							{if(k==1)
+								recordBuff.append("(select id from gene where name ='"+csvData[k] + "' and run_combination_id="+runCombIdList.get(i)+"),");
+							else	
+							recordBuff.append("'"+csvData[k]+"',");
+							}
+							recordBuff.append( runCombIdList.get(i) + ")");
 							fw.write(recordBuff.toString());
-							
+
 							recordBuff.setLength(0);
 						}
 
@@ -182,106 +174,96 @@ public class XLReader {
 					br.close();
 				}
 			}
-			
-//Writing to GeneGeneCategory 
-			ResultSet gst=geneStmt.executeQuery("SELECT * FROM gene;"); 
-			int i=1;
+
+			// Writing to GeneGeneCategory
+			ResultSet gst = geneStmt.executeQuery("SELECT * FROM gene;");
+			int i = 1;
 			fw.newLine();
-			while(gst.next())  
-			{
-				fw.write("insert into gene_genecategory (`id`,`genecategory_id`,`gene_id`)values ("+ i++ + ","+"(select id from genecategory where category_name='"+ geneSet.get(gst.getString(2))+"')"+ ","+ gst.getInt(1)+");");
+			while (gst.next()) {
+				fw.write("insert into gene_genecategory (`id`,`genecategory_id`,`gene_id`)values (" + i++ + ","
+						+ "(select id from genecategory where category_name='" + geneSet.get(gst.getString(2)) + "')"
+						+ "," + gst.getInt(1) + ");");
 				fw.newLine();
 			}
+			// Writing after Target table and Gene_genecategory entries
+			for (File file : listOfFiles) {
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				if (file.isFile()) {
+
+					if (file.getName().equalsIgnoreCase(endSqlFileName)) {
+						System.out.println("writing " + file.getName());
+						while ((line1 = br.readLine()) != null) {
+							fw.write(line1);
+							fw.newLine();
+
+						} // while end
+
+					}
+				} // If file End
+				br.close();
+			}
+			RandomGen addSampleQueries = new RandomGen();
+			addSampleQueries.writeExcelContentToSQlFile(fw);
 			con.close();
-			fw.close(); 
-		} 
-		catch (Exception e) {
+			fw.close();
+		} catch (Exception e) {
 			System.out.println(e);
-		} 
+		}
 
-		/*Target CSV update        
-
-       String csvFile = "/home/pradyumnm/Downloads/targetlistgeneslist/targetList.csv";
-       String line = "";
-       String cvsSplitBy = ",";
-       String fileTarget = "targetNtxt";
-       StringBuffer colNameBuff = new StringBuffer("");
-       StringBuffer recordBuff = new StringBuffer("");
-       BufferedWriter rw = new BufferedWriter(new FileWriter(fileTarget)) ;
-
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-        	 String columnList []= br.readLine().split(cvsSplitBy);
-        	 colNameBuff.append("INSERT INTO target (");
-        	 colNameBuff.append(columnList[0]);
-        	 int j=0;
-        	for(int i=1;i<columnList.length;i++)
-        		colNameBuff.append(","+columnList[i]);
-        	colNameBuff.append(") VALUES (");
-           while ((line = br.readLine()) != null) {
-
-                // use comma as separator
-            	recordBuff.append(colNameBuff);
-                String[] records = line.split(cvsSplitBy);
-                recordBuff.append(records[0]);
-            	for(int i=1;i<records.length;i++)
-            		recordBuff.append(","+records[i]);
-
-            	recordBuff.append(");");
-
-
-            	rw.write(recordBuff.toString());
-            	rw.newLine();
-
-            	recordBuff.setLength(0);
-            }
-            System.out.print(j);
-            rw.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-       // workbook.close();
-        inputStream.close();
+		/*
+		 * Target CSV update
+		 * 
+		 * String csvFile =
+		 * "/home/pradyumnm/Downloads/targetlistgeneslist/targetList.csv"; String line =
+		 * ""; String cvsSplitBy = ","; String fileTarget = "targetNtxt"; StringBuffer
+		 * colNameBuff = new StringBuffer(""); StringBuffer recordBuff = new
+		 * StringBuffer(""); BufferedWriter rw = new BufferedWriter(new
+		 * FileWriter(fileTarget)) ;
+		 * 
+		 * try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+		 * String columnList []= br.readLine().split(cvsSplitBy);
+		 * colNameBuff.append("INSERT INTO target (");
+		 * colNameBuff.append(columnList[0]); int j=0; for(int
+		 * i=1;i<columnList.length;i++) colNameBuff.append(","+columnList[i]);
+		 * colNameBuff.append(") VALUES ("); while ((line = br.readLine()) != null) {
+		 * 
+		 * // use comma as separator recordBuff.append(colNameBuff); String[] records =
+		 * line.split(cvsSplitBy); recordBuff.append(records[0]); for(int
+		 * i=1;i<records.length;i++) recordBuff.append(","+records[i]);
+		 * 
+		 * recordBuff.append(");");
+		 * 
+		 * 
+		 * rw.write(recordBuff.toString()); rw.newLine();
+		 * 
+		 * recordBuff.setLength(0); } System.out.print(j); rw.close(); } catch
+		 * (FileNotFoundException e) { e.printStackTrace(); } catch (IOException e) {
+		 * e.printStackTrace(); }
+		 * 
+		 * // workbook.close(); inputStream.close();
 		 */
-		
-//-------------------------------------Excel reading 
-		/*       String excelFilePath = "depth_config_stats.xlsx";
-        String fileName = "filename.sql";
-        FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
 
-        Workbook workbook = new XSSFWorkbook(inputStream);
-        Sheet firstSheet = workbook.getSheetAt(0);
-        Iterator<Row> iterator = firstSheet.iterator();
-        BufferedWriter bw = new BufferedWriter(new FileWriter(fileName)) ;
-        while (iterator.hasNext()) {
-            Row nextRow = iterator.next();
-            Iterator<Cell> cellIterator = nextRow.cellIterator();
-
-            while (cellIterator.hasNext()) {
-                Cell cell = cellIterator.next();
-
-                switch (cell.getCellType()) {
-                    case Cell.CELL_TYPE_STRING:
-                        //System.out.print(cell.getStringCellValue());
-                    	bw.write(cell.getStringCellValue());
-                        break;
-                    case Cell.CELL_TYPE_BOOLEAN:
-                        System.out.print(cell.getBooleanCellValue());
-                        break;
-                    case Cell.CELL_TYPE_NUMERIC:
-                        System.out.print(cell.getNumericCellValue());
-                        break;
-                }
-                System.out.print(" - ");
-            }
-            System.out.println();
-        }
+		// -------------------------------------Excel reading
+		/*
+		 * String excelFilePath = "depth_config_stats.xlsx"; String fileName =
+		 * "filename.sql"; FileInputStream inputStream = new FileInputStream(new
+		 * File(excelFilePath));
+		 * 
+		 * Workbook workbook = new XSSFWorkbook(inputStream); Sheet firstSheet =
+		 * workbook.getSheetAt(0); Iterator<Row> iterator = firstSheet.iterator();
+		 * BufferedWriter bw = new BufferedWriter(new FileWriter(fileName)) ; while
+		 * (iterator.hasNext()) { Row nextRow = iterator.next(); Iterator<Cell>
+		 * cellIterator = nextRow.cellIterator();
+		 * 
+		 * while (cellIterator.hasNext()) { Cell cell = cellIterator.next();
+		 * 
+		 * switch (cell.getCellType()) { case Cell.CELL_TYPE_STRING:
+		 * //System.out.print(cell.getStringCellValue());
+		 * bw.write(cell.getStringCellValue()); break; case Cell.CELL_TYPE_BOOLEAN:
+		 * System.out.print(cell.getBooleanCellValue()); break; case
+		 * Cell.CELL_TYPE_NUMERIC: System.out.print(cell.getNumericCellValue()); break;
+		 * } System.out.print(" - "); } System.out.println(); }
 		 */
 	}
 
-
-
 }
-
