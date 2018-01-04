38dfc7082d147575311783c4fc26c8c3d4cb2066
PradyumnM <32297668+PradyumnM@users.noreply.github.com>
2018-01-04 13:43:14 +0530

    Update RandomGen.java

--- src/com/sema/util/RandomGen.java
+++ src/com/sema/util/RandomGen.java
@@ -4,9 +4,11 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Properties;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -26,7 +28,13 @@ void writeExcelContentToSQlFile(BufferedWriter fw, String type) throws IOExcepti
 	HashSet<String> idSet= new HashSet<String>();
 
 	try {
-		FileInputStream file = new FileInputStream(new File("/home/pradyumnm/Downloads/targetlistgeneslist/master config - controls - validation.xlsx"));
+		Properties prop = new Properties();
+		InputStream input = null;
+		input = new FileInputStream("config.properties");
+		prop.load(input);
+		String controlSampleFileName = prop.getProperty("controlsamplefileName");
+		File folder = new File(prop.getProperty("filedir"));
+		FileInputStream file = new FileInputStream(new File(folder+"/"+controlSampleFileName));
 
 		//Create Workbook instance holding reference to .xlsx file
 		XSSFWorkbook workbook = new XSSFWorkbook(file);
@@ -36,13 +44,13 @@ void writeExcelContentToSQlFile(BufferedWriter fw, String type) throws IOExcepti
 
 		//Iterate through each rows one by one
 		Iterator<Row> rowIterator = sheet.iterator();
-		String [] records = new String[13];
+		
 		int i=0;
 
-
+int flag=0;
 		while (rowIterator.hasNext())
 
-		{
+		{String [] records = new String[13];
 			Row row = rowIterator.next();
 			//For each row, iterate through all the columns
 			Iterator<Cell> cellIterator = row.cellIterator();
@@ -63,12 +71,22 @@ void writeExcelContentToSQlFile(BufferedWriter fw, String type) throws IOExcepti
 					System.out.println(cell.getStringCellValue());
 
 					break;
+				case Cell.CELL_TYPE_BLANK:
+					records[i++]=null;
+					break;
 				}
 
 			}
-			System.out.println("records"+" "+records[0]+" "+records[2]+" "+records[3]);
-			idSet.add(records[0]);
+			
+			
+			if(flag==1)
+			{
+				System.out.println("records"+" "+records[0]+" "+records[2]+" "+records[3]);
+				idSet.add(records[0]);
 			masterConfigMap.put(records[0]+"_"+records[1]+"_"+records[3]+"_"+records[4]+"_"+records[5], records);
+		//	System.out.println("recordsFull"+records);
+			}
+			flag=1;
 		}
 		file.close();
 		workbook.close();
@@ -134,9 +152,12 @@ void writeExcelContentToSQlFile(BufferedWriter fw, String type) throws IOExcepti
 			colNameBuff.append("INSERT INTO control_sample_variants_reference "+ "(");
 			colNameBuff.append("sample_id,target_name,chromosome,position,ref_allele,alt_allele,gene,cdna_change,protein_change,expected_al,expected_al_start,expected_al_end) VALUES ");
 			fw.write(colNameBuff.toString());
+			String s="";
 			for (String key : masterConfigMap.keySet()) {
-
+				//System.out.println("Key"+key);
+				
 				String[] records = masterConfigMap.get(key);
+			//	System.out.println("records"+" "+records[0]+" "+records[2]+" "+records[3]);
 				if(flag==0) 
 				{
 					recordBuff.append("(");
@@ -150,7 +171,8 @@ void writeExcelContentToSQlFile(BufferedWriter fw, String type) throws IOExcepti
 				recordBuff.append( "(select id from sample where lims_sample_id ='"+records[0]+"')");
 				recordBuff.append(","+"'"+records[1]+"'");
 				recordBuff.append(","+"'"+records[2]+"'");
-				recordBuff.append(","+"'"+records[3]+"'");
+				
+				recordBuff.append(","+"'"+records[3].replaceAll(",", "")+"'");
 				recordBuff.append(","+"'"+records[4]+"'");
 
 				recordBuff.append(","+"'"+records[5]+"'");
